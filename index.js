5d74b229997cdee6a687eabf08f198a18bdea159
Devastated God <vipin878707@gmail.com>
2020-12-18 16:26:30 +0530

    m

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "devastatedgod.github.io/inshare";
+const baseURL = "https://trail-inshare.herokuapp.com";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
